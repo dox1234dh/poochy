f69cf95783690a7f024ff4106a912930bc838f5d
Do Tran Van <dox1dh@gmail.com>
2026-05-30 17:43:00 +0700

    init: add some config

--- content.config.ts
+++ content.config.ts
@@ -1,10 +1,26 @@
-import { defineContentConfig, defineCollection } from '@nuxt/content'
+import { defineContentConfig, defineCollection, property } from '@nuxt/content'
+import { z } from 'zod'
 
 export default defineContentConfig({
   collections: {
     content: defineCollection({
       type: 'page',
       source: '**',
+      schema: z.object({
+        // validate
+        title: z.string(),
+        description: z.string().optional(),
+        updatedAt: z.date(),
+        draft: z.boolean().optional(),
+        tags: z.array(z.string()).optional(),
+        hero: z.object({
+          image: property(z.string()).editor({ input: 'media' }),
+          caption: z.string().optional()
+        }),
+        // component
+        // Reuse props from a dependency (path is resolved like an import)
+        button: property(z.object({})).inherit('@nuxt/ui/components/Button.vue')
+      })
     }),
   },
 })
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content', 'nuxt-studio'],
+  modules: ['@nuxt/ui', '@nuxt/content', 'nuxt-studio'],
+  css: ['~/assets/css/main.css'],
+  components: [
+    {
+      path: '~/app/components',
+      global: true,
+    }
+  ],
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
 })
